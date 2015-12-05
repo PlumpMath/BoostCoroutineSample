a502ac5d7b895f1414b2a5e616f552c7b8916c2b
kobake <kobake@users.sourceforge.net>
2015-12-05 13:55:31 +0900

    easy sample

--- src/FiberSample.cpp
+++ src/FiberSample.cpp
@@ -2,10 +2,35 @@
 //
 
 #include "stdafx.h"
+#include <Windows.h>
 
+LPVOID g_lpMainFiber;
+LPVOID g_lpSubFiber;
+
+void WINAPI SubFiberRoutine(LPVOID lpFiberParameter)
+{
+	printf("fiber1\n");
+	SwitchToFiber(g_lpMainFiber);
+	printf("fiber2\n");
+	SwitchToFiber(g_lpMainFiber);
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	ConvertThreadToFiber(NULL);
+
+	g_lpMainFiber = ::GetCurrentFiber();
+
+	printf("main1\n");
+	g_lpSubFiber = ::CreateFiber(0, SubFiberRoutine, NULL);
+	printf("main2\n");
+	SwitchToFiber(g_lpSubFiber);
+	printf("main3\n");
+	SwitchToFiber(g_lpSubFiber);
+	printf("main4\n");
+	SwitchToFiber(g_lpSubFiber);
+	printf("main5\n");
+
+
 	return 0;
 }
-
