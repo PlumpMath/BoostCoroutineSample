61ac7134e3bdfeea65bd87f9aa05631386632e40
kobake <kobake@users.sourceforge.net>
2015-12-05 15:06:41 +0900

    coroutineサンプル

--- src/FiberSample.cpp
+++ src/FiberSample.cpp
@@ -4,34 +4,46 @@
 #include "stdafx.h"
 #include <Windows.h>
 #include <boost/coroutine/coroutine.hpp>
+using namespace boost::coroutines;
 
-LPVOID g_lpMainFiber;
-LPVOID g_lpSubFiber;
-
-void WINAPI SubFiberRoutine(LPVOID lpFiberParameter)
+void loop(coroutine<int()>::caller_type& yield)
 {
-	printf("fiber1\n");
-	SwitchToFiber(g_lpMainFiber);
-	printf("fiber2\n");
-	SwitchToFiber(g_lpMainFiber);
+	for (int i = 0; i < 10; ++i) {
+		yield(i);
+	}
+}
+
+void loop2(coroutine<int()>::caller_type& yield)
+{
+	for (int i = 0; i < 10; ++i) {
+		yield(i * 100);
+	}
+}
+
+template<typename P>
+void enumerate(P predicate)
+{
+	coroutine<int()> routine(loop);
+	coroutine<int()> routine2(loop2);
+
+	while (routine || routine2) {
+		if (routine){
+			int i = routine.get();
+			predicate(i);
+			routine();
+		}
+		if (routine2){
+			int i = routine2.get();
+			predicate(i);
+			routine2();
+		}
+	}
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	ConvertThreadToFiber(NULL);
-
-	g_lpMainFiber = ::GetCurrentFiber();
-
-	printf("main1\n");
-	g_lpSubFiber = ::CreateFiber(0, SubFiberRoutine, NULL);
-	printf("main2\n");
-	SwitchToFiber(g_lpSubFiber);
-	printf("main3\n");
-	SwitchToFiber(g_lpSubFiber);
-	printf("main4\n");
-	SwitchToFiber(g_lpSubFiber);
-	printf("main5\n");
-
+	enumerate([](int i){ std::cout << i << " "; });
+	std::cout << std::endl;
 
 	return 0;
 }
