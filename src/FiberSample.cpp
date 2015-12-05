4a2b467a3895a0c78a91e1eb29e2e13236127384
kobake <kobake@users.sourceforge.net>
2015-12-05 16:28:25 +0900

    coroutineにパラメータを渡す

--- src/FiberSample.cpp
+++ src/FiberSample.cpp
@@ -3,11 +3,60 @@
 
 #include "stdafx.h"
 #include <Windows.h>
+#include <list>
+#include <vector>
 #include <boost/coroutine/coroutine.hpp>
 using namespace boost::coroutines;
+/*
+class Coroutine{
+private:
+	static void s_loop(coroutine<int()>::caller_type& yield){
+		
+	}
+public:
+	Coroutine();
+	virtual void run() = 0;
+	void doContinue(){
+		if (m_coroutine){
+			m_coroutine();
+		}
+	}
+protected:
+	void wait(){
+		return;
+	}
+private:
+	coroutine<int()> m_coroutine;
+};
 
-void loop(coroutine<int()>::caller_type& yield)
+class CoroutineManager{
+public:
+	static CoroutineManager* instance(){
+		static CoroutineManager theInstance;
+		return &theInstance;
+	}
+public:
+	void add(Coroutine* coroutine){
+		m_coroutines.push_back(coroutine);
+	}
+	void go(){
+		for (Coroutine* coroutine : m_coroutines){
+			coroutine->doContinue();
+		}
+	}
+private:
+	std::list<Coroutine*> m_coroutines;
+};
+
+Coroutine::Coroutine()
+: m_coroutine(s_loop)
 {
+	CoroutineManager::instance()->add(this);
+}
+*/
+void loop(coroutine<int()>::caller_type& yield, int param)
+{
+	printf("param = %d\n", param);
 	for (int i = 0; i < 10; ++i) {
 		yield(i);
 	}
@@ -23,7 +72,8 @@ void loop2(coroutine<int()>::caller_type& yield)
 template<typename P>
 void enumerate(P predicate)
 {
-	coroutine<int()> routine(loop);
+	using std::placeholders::_1;
+	coroutine<int()> routine( std::bind(loop, _1, 2000) );
 	coroutine<int()> routine2(loop2);
 
 	while (routine || routine2) {
